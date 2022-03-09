3f6aca7f99eca8231a9334f8b5ccae69af0a5762
rahul.pandey <Nayan@1990>
2022-03-09 23:15:18 +0530

    Initial commit

--- src/test/java/org/example/ExcelToDataBase.java
+++ src/test/java/org/example/ExcelToDataBase.java
@@ -5,40 +5,39 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.Test;
 
-import java.io.FileNotFoundException;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.sql.*;
 
-public class DataBaseToExcel {
+public class ExcelToDataBase {
     Connection con;
-    FileOutputStream fos;
     XSSFWorkbook workbook;
+    FileInputStream fis;
     @Test
     public void dbToExcel(){
         try{
             con= DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/exceldemo","root","Nayan@1990");
             Statement st=con.createStatement();
-            ResultSet rs=st.executeQuery("SELECT * FROM exceldemo.EmpDetails");
-            workbook=new XSSFWorkbook();
-            XSSFSheet sheet=workbook.createSheet("dbDemo");
-            XSSFRow row=sheet.createRow(0);
-            row.createCell(0).setCellValue("id");
-            row.createCell(1).setCellValue("Name");
-            row.createCell(2).setCellValue("org");
-            int i=1;
-            while (rs.next()){
-                double id=rs.getDouble("id");
-                String name=rs.getString("Name");
-                String org=rs.getString("org");
-                row=sheet.createRow(i++);
-                row.createCell(0).setCellValue(id);
-                row.createCell(1).setCellValue(name);
-                row.createCell(2).setCellValue(org);
+            //Create a new table 'MyDetails' in database
+            String sql="CREATE TABLE exceldemo.MyDetails(id decimal(4,0),Name VARCHAR(45),org VARCHAR(45))";
+            st.execute(sql);
+            //Read data from excel
+            fis=new FileInputStream("demo_dbTo_Excel.xlsx");
+            workbook=new XSSFWorkbook(fis);
+            XSSFSheet sheet=workbook.getSheetAt(0);
+            int rows=sheet.getPhysicalNumberOfRows();
+
+            for(int i=1;i<rows;i++){
+                XSSFRow row=sheet.getRow(i);
+                double id=row.getCell(0).getNumericCellValue();
+                String name=row.getCell(1).getStringCellValue();
+                String org=row.getCell(2).getStringCellValue();
+                sql="insert into exceldemo.MyDetails values ('"+id+"','"+name+"','"+org+"')";
+                st.execute(sql);
+                st.execute("commit");
             }
-            fos=new FileOutputStream("demo_dbTo_Excel.xlsx");
-            workbook.write(fos);
         }catch (SQLException sqe){
             System.out.println(sqe.getMessage());
         }catch (IOException ie){
@@ -51,7 +50,7 @@ public class DataBaseToExcel {
                 e.printStackTrace();
             }
             try {
-                fos.close();
+                fis.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
